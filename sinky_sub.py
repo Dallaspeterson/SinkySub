46f21c107690659e81ff8e65c8e3918c32178236
Dallas Peterson <dallasrollinpeterson@gmail.com>
2023-08-03 02:45:04 -0700

    Update sinky_sub.py: Add additional functionality

--- sinky_sub.py
+++ sinky_sub.py
@@ -1,9 +1,13 @@
+def to_lowercase(text):
+    # Convert the text to lowercase
+    return text.lower()
+
 def main():
     # Get input from the user
     user_input = input("Enter some text: ")
 
     # Convert the input to lowercase
-    lowercase_text = user_input.lower()
+    lowercase_text = to_lowercase(user_input)
 
     # Print the lowercase text
     print("Lowercase text:", lowercase_text)
