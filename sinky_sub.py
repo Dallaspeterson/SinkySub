210e070f00425c6c92c17d8cb876f23e29eb7494
Dallas Peterson <dallasrollinpeterson@gmail.com>
2023-08-03 02:27:40 -0700

    Add Python script and update README.md

---
+++ sinky_sub.py
@@ -0,0 +1,12 @@
+def main():
+    # Get input from the user
+    user_input = input("Enter some text: ")
+
+    # Convert the input to lowercase
+    lowercase_text = user_input.lower()
+
+    # Print the lowercase text
+    print("Lowercase text:", lowercase_text)
+
+if __name__ == "__main__":
+    main()
