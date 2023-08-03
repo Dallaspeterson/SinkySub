5187b4fea19b2bdf389b942fac8cfd75d4fc6a2e
Dallas Peterson <dallasrollinpeterson@gmail.com>
2023-08-03 03:05:01 -0700

    Add to_lowercase function to convert text to lowercase

--- sinky_sub.py
+++ sinky_sub.py
@@ -1,16 +1,19 @@
-def to_lowercase(text):
-    # Convert the text to lowercase
-    return text.lower()
+def reverse_text(text):
+    return text[::-1]
 
 def main():
     # Get input from the user
     user_input = input("Enter some text: ")
 
     # Convert the input to lowercase
-    lowercase_text = to_lowercase(user_input)
+    lowercase_text = user_input.lower()
 
     # Print the lowercase text
     print("Lowercase text:", lowercase_text)
+
+    # Reverse the text and print it
+    reversed_text = reverse_text(user_input)
+    print("Reversed text:", reversed_text)
 
 if __name__ == "__main__":
     main()
